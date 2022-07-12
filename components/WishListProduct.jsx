048b7a8c78e9845c016c97f9d334723160dcb7c6
omarelkamah <omarwork010@gmail.com>
2022-07-12 04:52:07 +0200

    Finish WishList Responsive Page

--- components/WishListProduct.jsx
+++ components/WishListProduct.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 
 const WishListProduct = () => {
   return (
-    <div className='flex flex-wrap items-center justify-around gap-3 mb-6 hover:shadow-lg transition-all'>
+    <div className='flex flex-col sm:flex-row flex-wrap items-center justify-around gap-3 mb-6 hover:shadow-lg transition-all py-4'>
       <div className='relative'>
         <Image
           src='/images/banner.png'
@@ -26,7 +26,7 @@ const WishListProduct = () => {
           CATEGORY:<span className='text-sm text-backLightDark'> makeup</span>
         </p>
       </div>
-      <div className='w-fit flex flex-col items-center gap-8'>
+      <div className='w-fit flex flex-col items-center gap-4 sm:gap-8'>
         <Button title='add to cart' />
         <Button
           title='remove from wishlist'
