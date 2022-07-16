bea010da396bd27eb1122f5059a40b21d03746ab
omarelkamah <omarwork010@gmail.com>
2022-07-16 15:33:12 +0200

    Finish About component responsive

--- components/home/Gift.jsx
+++ components/home/Gift.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Gift = () => {
   return (
-    <div className='w-[70%] mx-auto mr-11 my-16 px-4 py-12 gap-5 bg-backYellow flex'>
+    <div className='lg:w-[70%] p-3 mx-auto lg:mr-11 my-16 px-4 py-12 gap-5 bg-backYellow flex flex-col md:flex-row items-center justify-center'>
       <div className='relative'>
         <div className='absolute -left-16'>
           <Image
@@ -30,11 +30,13 @@ const Gift = () => {
           type='email'
           placeholder='Your email'
           className='inline-block mb-8 p-3 py-2 outline-none bg-transparent border-b-[1px] w-full border-backDark'
+          required
         />
         <Button
           title='subscribe'
           bgColor='bg-backDark'
           textColor='text-white'
+          type='submit'
         />
       </div>
     </div>
