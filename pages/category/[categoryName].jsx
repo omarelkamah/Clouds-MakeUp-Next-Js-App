bbc609855ff71ba69653ddb01de338464cc10909
omarelkamah <omarwork010@gmail.com>
2022-07-23 14:40:54 +0200

    without category page

--- pages/category/[categoryName].jsx
+++ pages/category/[categoryName].jsx
@@ -2,37 +2,37 @@ import React from 'react'
 import Product from '../../components/product/Product'
 import Poster from '../../components/ui/Poster'
 
-export const getStaticPaths = async () => {
-  const res = await fetch(
-    'https://makeup-api.herokuapp.com/api/v1/products.json'
-  )
-  const products = await res.json()
+// export const getStaticPaths = async () => {
+//   const res = await fetch(
+//     'https://makeup-api.herokuapp.com/api/v1/products.json'
+//   )
+//   const products = await res.json()
 
-  const paths = products.map(product => {
-    return {
-      params: { categoryName: String(product.category) }
-    }
-  })
+//   const paths = products.map(product => {
+//     return {
+//       params: { categoryName: String(product.category) }
+//     }
+//   })
 
-  return { paths, fallback: false }
-}
+//   return { paths, fallback: false }
+// }
 
-export const getStaticProps = async context => {
-  const categoryName = context.params.categoryName
-  const res = await fetch(
-    `https://makeup-api.herokuapp.com/api/v1/products.json?product_category=${categoryName}`
-  )
-  const products = await res.json()
+// export const getStaticProps = async context => {
+//   const categoryName = context.params.categoryName
+//   const res = await fetch(
+//     `https://makeup-api.herokuapp.com/api/v1/products.json?product_category=${categoryName}`
+//   )
+//   const products = await res.json()
 
-  return { props: { products } }
-}
+//   return { props: { products } }
+// }
 
 const Category = ({ products }) => {
   return (
     <div>
       <Poster bottom='0' />
       <div className='flex flex-wrap justify-center mt-6'>
-        {products.map(product => (
+        {/* {products.map(product => (
           <Product
             key={product.id}
             id={product.id}
@@ -41,7 +41,7 @@ const Category = ({ products }) => {
             price={product.price}
             category={product.category}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   )
